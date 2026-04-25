9c5fd7426582566e2cd673e87f8f5676eef0d75e
Erlon <32484089+ErlonRachi@users.noreply.github.com>
2026-04-25 09:46:15 +0200

    Add review comments to versao_exemplo_z1.R

--- versao_exemplo_z1.R
+++ versao_exemplo_z1.R
@@ -1,2 +1,3 @@
-# esta é a primeira versao
+## This is the Review of the previous Version ##
+# A primeira versão foi feita em 2021, provavelmente quando ainda estava no Brasil #
 # adicionei uma nova versão que eu preciso adicionar
